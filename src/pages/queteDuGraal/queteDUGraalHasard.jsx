ffcda18bd98997f05453095b9b3fcea18be5e018
Bossola-Yannick <bossolay@hotmail.fr>
2025-06-30 11:23:37 +0200

    feat: added hasard dice for "Quete du Graal"

--- src/pages/queteDuGraal/queteDUGraalHasard.jsx
+++ src/pages/queteDuGraal/queteDUGraalHasard.jsx
@@ -1,1 +1,26 @@
-export default function QdGHasard() {}
+import Button from "../../components/Button/button";
+import { UsePersistedState } from "../../hooks/usePersistedState";
+import randomNumber from "../../utils/randomNumberLs";
+export default function QdGHasard() {
+  const [dice1, setDice1] = UsePersistedState("random-number", 0);
+  const [dice2, setDice2] = UsePersistedState("random-number", 0);
+
+  return (
+    <div className="qdg-hasardMain">
+      <h3 className="qdg-hasard-title">Prêt à faire confiance au hasard ?</h3>
+      <Button
+        className={"qdg-hasard-button"}
+        onClick={() => {
+          randomNumber(1, 6, setDice1);
+          randomNumber(1, 6, setDice2);
+        }}
+      >
+        Hasard Number
+      </Button>
+      <div className="qdg-dice-box">
+        <p className="qdg-hasard-score">{dice1}</p>
+        <p className="qdg-hasard-score">{dice2}</p>
+      </div>
+    </div>
+  );
+}
